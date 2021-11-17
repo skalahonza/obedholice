3fb6d366be7840b31e3443131683eab82939eceb
Jan Skála <j.skala@hotmail.com>
2021-11-17 12:45:26 +0100

    Re-factor code.

--- backend/ObedyHolice.BL/Services/Restaurants/CoronaHolice.cs
+++ backend/ObedyHolice.BL/Services/Restaurants/CoronaHolice.cs
@@ -2,30 +2,8 @@
 
 using System.Web;
 
-using ObedyHolice.BL.Interfaces;
-
 namespace ObedyHolice.BL.Services.Restaurants
 {
-    public abstract class RestaurantBase : IRestaurant
-    {
-        protected readonly Dictionary<string, DayOfWeek> daysCsToEn = new(StringComparer.OrdinalIgnoreCase)
-        {
-            { "Pondělí", DayOfWeek.Monday },
-            { "Úterý", DayOfWeek.Tuesday },
-            { "Středa", DayOfWeek.Wednesday },
-            { "Čtvrtek", DayOfWeek.Thursday },
-            { "Pátek", DayOfWeek.Friday },
-            { "Sobota", DayOfWeek.Saturday },
-            { "Neděle", DayOfWeek.Sunday }
-        };
-
-        public abstract string Name { get; }
-
-        public abstract string SubTitle { get; }
-
-        public abstract Task<List<string>> GetMenuItems();
-    }
-
     public class CoronaHolice : RestaurantBase
     {
         public override string Name => "Corona";
---
+++ backend/ObedyHolice.BL/Services/Restaurants/RestaurantBase.cs
@@ -0,0 +1,25 @@
+﻿
+using ObedyHolice.BL.Interfaces;
+
+namespace ObedyHolice.BL.Services.Restaurants
+{
+    public abstract class RestaurantBase : IRestaurant
+    {
+        protected readonly Dictionary<string, DayOfWeek> daysCsToEn = new(StringComparer.OrdinalIgnoreCase)
+        {
+            { "Pondělí", DayOfWeek.Monday },
+            { "Úterý", DayOfWeek.Tuesday },
+            { "Středa", DayOfWeek.Wednesday },
+            { "Čtvrtek", DayOfWeek.Thursday },
+            { "Pátek", DayOfWeek.Friday },
+            { "Sobota", DayOfWeek.Saturday },
+            { "Neděle", DayOfWeek.Sunday }
+        };
+
+        public abstract string Name { get; }
+
+        public abstract string SubTitle { get; }
+
+        public abstract Task<List<string>> GetMenuItems();
+    }
+}
