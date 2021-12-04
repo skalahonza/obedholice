f7bce98f09ffa6ab3da952b3ad8e2c870234f43c
Jan Skála <j.skala@hotmail.com>
2021-12-04 09:52:16 +0100

    Base url for production.

--- frontend/src/api/restaurants.js
+++ frontend/src/api/restaurants.js
@@ -1,8 +1,14 @@
+function baseUrl(){
+    return window.location.hostname == 'localhost'
+        ? 'http://localhost:7071'
+        : 'https://obedholice.azurewebsites.net';
+}
+
 /**
  * Get all restaurant menus.
  */
  async function getRestaurants() {
-    const response = await fetch('http://localhost:7071/api/restaurants');
+    const response = await fetch(baseUrl() + '/api/restaurants');
     return response.json();
 }
 
